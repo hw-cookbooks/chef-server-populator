277a89621b9b1a5c540f437fb72d09e93c446541
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-07 11:42:11 -0800

    Break options out from command

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,14 @@
 
-knife_cmd = "#{node[:chef_server_populator][:knife_exec]} " <<
-  "-k #{node[:chef_server_populator][:pem]} " <<
+knife_cmd = "#{node[:chef_server_populator][:knife_exec]}"
+knife_opts = "-k #{node[:chef_server_populator][:pem]} " <<
   "-u #{node[:chef_server_populator][:user]} " <<
   "-s https://127.0.0.1"
 pg_cmd = "/opt/chef-server/embedded/bin/psql -d opscode_chef"
 
 node[:chef_server_populator][:clients].each do |client, pub_key|
   execute "create client: #{client}" do
-    command "#{knife_cmd} client create --admin #{client}"
-    not_if "#{knife_cmd} client list | tr -d ' ' | grep '^#{client}$'"
+    command "#{knife_cmd} client create #{client} --admin #{knife_opts}"
+    not_if "#{knife_cmd} client list #{knife_opts}| tr -d ' ' | grep '^#{client}$'"
   end
   if(pub_key && File.directory?(node[:chef_server_populator][:base_path]))
     pub_key_path = File.join(node[:chef_server_populator][:base_path], pub_key)
