714ca58ec5768e32b006adc86f8a598c1dfeaefb
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-07 11:40:12 -0800

    Check that base path exists before processing

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,8 @@ node[:chef_server_populator][:clients].each do |client, pub_key|
     command "#{knife_cmd} client create --admin #{client}"
     not_if "#{knife_cmd} client list | tr -d ' ' | grep '^#{client}$'"
   end
-  if(pub_key)
-    pub_key_path = File.join(node[:chef_server_populator][:base_dir], pub_key)
+  if(pub_key && File.directory?(node[:chef_server_populator][:base_path]))
+    pub_key_path = File.join(node[:chef_server_populator][:base_path], pub_key)
     if(File.exists?(pub_key_path))
       execute "set public key for: #{client}" do
         command "#{pg_cmd} -c \"update clients set public_key = E'#{File.read(pub_key_path)}' where name = '#{client}'\""
