bffb135769c2c852dcea899b06c3047d5884b993
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-07 11:34:48 -0800

    Build full path to file

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,10 +10,13 @@ node[:chef_server_populator][:clients].each do |client, pub_key|
     command "#{knife_cmd} client create --admin #{client}"
     not_if "#{knife_cmd} client list | tr -d ' ' | grep '^#{client}$'"
   end
-  if(pub_key && File.exists?(pub_key))
-    execute "set public key for: #{client}" do
-      command "#{pg_cmd} -c \"update clients set public_key = E'#{File.read(pub_key)}' where name = '#{client}'\""
-      user 'opscode-pgsql'
+  if(pub_key)
+    pub_key_path = File.join(node[:chef_server_populator][:base_dir], pub_key)
+    if(File.exists?(pub_key_path))
+      execute "set public key for: #{client}" do
+        command "#{pg_cmd} -c \"update clients set public_key = E'#{File.read(pub_key_path)}' where name = '#{client}'\""
+        user 'opscode-pgsql'
+      end
     end
   end
 end
