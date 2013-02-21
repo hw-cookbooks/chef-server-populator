34f27e6130cbcfa533b3b5988bf9f40bd61f5749
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-21 12:02:06 -0800

    Add recipe for solo use

---
+++ recipes/solo.rb
@@ -0,0 +1,36 @@
+if(node[:chef_server_populator][:servername_override])
+  node.set[:chef_server][:nginx][:server_name] = node[:chef_server_populator][:servername_override]
+  node.set[:chef_server][:bookshelf][:vip] = node[:chef_server_populator][:servername_override]
+  node.set[:chef_server][:lb][:api_fqdn] = node[:chef_server_populator][:servername_override] 
+  node.set[:chef_server][:lb][:web_ui_fqdn] = node[:chef_server_populator][:servername_override] 
+else
+  node.set[:chef_server][:nginx][:server_name] = node[:fqdn]
+  node.set[:chef_server][:bookshelf][:vip] = node[:fqdn]
+  node.set[:chef_server][:lb][:api_fqdn] = node[:fqdn] 
+  node.set[:chef_server][:lb][:web_ui_fqdn] = node[:fqdn] 
+end
+
+include_recipe 'chef-server'
+
+knife_cmd = "#{node[:chef_server_populator][:knife_exec]}"
+knife_opts = "-k #{node[:chef_server_populator][:pem]} " <<
+  "-u #{node[:chef_server_populator][:user]} " <<
+  "-s https://127.0.0.1"
+pg_cmd = "/opt/chef-server/embedded/bin/psql -d opscode_chef"
+
+node[:chef_server_populator][:clients].each do |client, pub_key|
+  execute "create client: #{client}" do
+    command "#{knife_cmd} client create #{client} --admin -d #{knife_opts}"
+    not_if "#{knife_cmd} client list #{knife_opts}| tr -d ' ' | grep '^#{client}$'"
+  end
+  if(pub_key && File.directory?(node[:chef_server_populator][:base_path]))
+    pub_key_path = File.join(node[:chef_server_populator][:base_path], pub_key)
+    if(File.exists?(pub_key_path))
+      pub_key_con = File.read(pub_key_path)
+      execute "set public key for: #{client}" do
+        command "#{pg_cmd} -c \"update clients set public_key = E'#{pub_key_con}' where name = '#{client}'\""
+        user 'opscode-pgsql'
+      end
+    end
+  end
+end
