eb982ab8f904aca72462e0f87907b7465f15ba7a
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-21 14:39:56 -0800

    Vendor cookbook into embedded

--- recipes/configurator.rb
+++ recipes/configurator.rb
@@ -4,7 +4,31 @@ execute 'reconfigure chef server!' do
   command 'chef-server-ctl reconfigure'
 end
 
-file "/opt/chef-server/embedded/cookbooks/dna.json" do
+populator_path = '/opt/chef-server/embedded/cookbooks/chef-server-populator'
+
+directory populator_path do
+  action :delete
+  only_if do
+    File.directory?(populator_path) &&
+      !FileUtils.cmp(
+        File.join(populator_path, 'metadata.rb'), 
+        File.join(Chef::Config[:cookbook_path], 'chef-server-populator/metadata.rb')
+      )
+  end
+end
+
+ruby_block 'vendor chef-server-populator' do
+  block do
+    FileUtils.cp_r(
+      File.join(Chef::Config[:cookbook_path], 'chef-server-populator'), populator_path
+    )
+  end
+  not_if do
+    File.directory?(populator_path)
+  end
+end
+
+file '/opt/chef-server/embedded/cookbooks/dna.json' do
   mode 0644
   content(
     JSON.pretty_generate(
