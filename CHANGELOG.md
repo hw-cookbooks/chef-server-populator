e439f71afc5e06342cf8571a0d93cdb13f95ee12
Chris Roberts <chrisroberts.code@gmail.com>
2013-02-21 16:14:17 -0800

    Bump version 0.2.0 and add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## v0.2.0
+
+* Provide distinct solo vs. client recipes
+* Client recipe configures dna.json and uses ctl for reconfigure
--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ name 'chef-server-populator'
 description 'Populate chef server with stuff you want'
 maintainer 'Heavywater'
 maintainer_email 'support@hw-ops.com'
-version '0.1.1'
+version '0.2.0'
 
