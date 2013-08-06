f130a48f7c375b7c8ffea3b57326e1a20348e300
Chris Roberts <chrisroberts.code@gmail.com>
2013-08-06 06:36:20 -0700

    Include url in configurator for bookshelf

--- recipes/configurator.rb
+++ recipes/configurator.rb
@@ -8,12 +8,14 @@ if(node[:chef_server_populator][:endpoint])
     node.set['chef-server'][:configuration][:bookshelf][:vip] =
     node.set['chef-server'][:configuration][:lb][:api_fqdn] =
     node.set['chef-server'][:configuration][:lb][:web_ui_fqdn] = node[:chef_server_populator][:endpoint]
+  node.set['chef-server'][:configuration][:bookshelf][:url] = "https://#{node[:chef_server_populator][:endpoint]}"
 else
   node.set['chef-server'][:api_fqdn] =
     node.set['chef-server'][:configuration][:nginx][:server_name] =
     node.set['chef-server'][:configuration][:bookshelf][:vip] =
     node.set['chef-server'][:configuration][:lb][:api_fqdn] =
     node.set['chef-server'][:configuration][:lb][:web_ui_fqdn] = node[:fqdn]
+  node.set['chef-server'][:configuration][:bookshelf][:url] = "https://#{node[:fqdn]}"
 end
 
 include_recipe 'chef-server'
