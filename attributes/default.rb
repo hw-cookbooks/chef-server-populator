ddb9af5bb58e5419a7c6e3c4a66d22b68f3d1db4
Michael F. Weinberg <michael@luckymike.com>
2014-02-24 15:03:13 -0800

    Move backups to a script so they get their own cron

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,8 +20,14 @@ default[:chef_server_populator][:cookbook_auto_install] = true
 # ref: https://tickets.opscode.com/browse/CHEF-3838
 default[:chef_server_populator][:force_init] = false # upstart or sysvinit
 
-default[:chef_server_populator][:backup][:dir] = '/tmp/chef-server/backup'
-default[:chef_server_populator][:backup][:file] = 'chef-server.dump'
 default[:chef_server_populator][:restore][:file] = nil
 default[:chef_server_populator][:restore][:local_path] = '/tmp/chef-server.dump'
+
+default[:chef_server_populator][:backup][:dir] = '/tmp/chef-server/backup'
+default[:chef_server_populator][:backup][:file] = 'chef-server.dump'  
+default[:chef_server_populator][:backup][:remote][:connection] = nil
+default[:chef_server_populator][:backup][:schedule] = {
+  :minute => '33',
+  :hour => '3'
+}
 
--- recipes/restore.rb
+++ recipes/restore.rb
@@ -8,9 +8,26 @@ else
   file = node[:chef_server_populator][:restore][:file]
 end
 
+execute "backup chef server stop" do
+  command "chef-server-ctl stop"
+  creates '/etc/chef-server/restore.json'
+end
+
 #Restore nodes from file
 execute "restoring chef data" do
-  command "/opt/chef-server/embedded/bin/pg_restore -d opscode_chef #{file}"
+  command "/opt/chef-server/embedded/bin/pg_restore --clean --exit-on-error --dbname=opscode_chef #{file}"
   user 'opscode-pgsql'
+  creates '/etc/chef-server/restore.json'
 end
-node.set[:chef_server_populator][:restore][:file] = nil
+
+execute "backup chef server start" do
+  command "chef-server-ctl start"
+  creates '/etc/chef-server/restore.json'
+end
+
+directory '/etc/chef-server'
+
+file '/etc/chef-server/restore.json' do
+  content JSONCompat.to_json_pretty(:date => Time.now.to_i,
+                                    :file => file)
+end
