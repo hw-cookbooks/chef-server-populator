db8a2b8a463803cc5fae06a9e92ac602574fb638
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-02 10:16:06 -0700

    Update version for development 0.3.3

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name 'chef-server-populator'
 description 'Populate chef server with stuff you want'
 maintainer 'Heavywater'
 maintainer_email 'support@hw-ops.com'
-version '0.3.2'
+version '0.3.3'
 
 depends 'chef-server', '>= 2.1.0'
