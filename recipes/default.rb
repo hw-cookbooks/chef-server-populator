01ef404f6c81ec7672e0ca9b8267e0ec536e7ea1
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-05 14:33:44 -0700

    Move restore recipe below general setup

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
-if(!node[:chef_server_populator][:restore][:file].empty? &&
-    node[:chef_server_populator][:restore][:file] != 'none')
-  include_recipe 'chef-server-populator::restore'
-end
-
 if(Chef::Config[:solo])
   include_recipe 'chef-server-populator::solo'
 else
   include_recipe 'chef-server-populator::client'
+end
+
+if(!node[:chef_server_populator][:restore][:file].empty? &&
+    node[:chef_server_populator][:restore][:file] != 'none')
+  include_recipe 'chef-server-populator::restore'
 end
 
 if((%w(amazon xenserver).include?(node.platform) && node.platform_version.to_i >= 6) || node.platform == 'fedora')
