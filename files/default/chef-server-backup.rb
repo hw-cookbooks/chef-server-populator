39df21cf0df846fed12d358fda3c1f693dfd4351
Chris Roberts <chrisroberts.code@gmail.com>
2014-08-05 10:02:45 -0700

    Modify backup from template to file. Use configuration file on system.

---
+++ files/default/chef-server-backup.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+
+require 'fog'
+require 'multi_json'
+require 'mixlib/shellout'
+
+DEFAULT_CONFIGURATION_PATH = '/etc/chef-server/backup.json'
+
+if(ARGV.size > 1 || !File.exists?(ARGV.first.to_s))
+  puts 'Usage: chef-server-backup CONFIG_FILE_PATH'
+  exit
+else
+  config = MultiJson.load(
+    ARGV.first || DEFAULT_CONFIGURATION_PATH,
+    :symbolize_keys => true
+  )
+end
+
+server_manifest = MultiJson.load(
+  File.read('/opt/chef-server/version-manifest.txt'),
+  :symbolize_keys => true
+)
+
+prefix = [
+  Time.now.to_i,
+  "ver_#{server_manifest[:version]}",
+  config[:filename]
+].join('-')
+
+db_file = File.join(
+  config[:dir],
+  "#{prefix}.dump"
+)
+
+data_file = File.join(
+  config[:dir],
+  "#{prefix}.tar.gz"
+)
+
+backup = Mixlib::ShellOut.new([
+    'sudo -i -u opscode-pgsql /opt/chef-server/embedded/bin/pg_dump',
+    "opscode_chef --username=opscode-pgsql --format=custom -f #{db_file}"
+  ].join(' ')
+)
+
+backup.run_command
+backup.error!
+
+backup_data = Mixlib::ShellOut.new(
+  "tar -czf #{data_file} -C /var/opt/chef-server/bookshelf data"
+)
+backup_data.run_command
+backup_data.error!
+
+remote_creds = [:remote, :connection].inject(config) do |memo, key|
+  memo[key] || break
+end
+remote_directory = [:remote, :directory].inject(config) do |memo, key|
+  memo[key] || break
+end
+
+if(remote_creds)
+  if(remote_directory)
+    remote = Fog::Storage.new(remote_creds)
+    directory = remote.directories.create(:key => remote_directory)
+    [db_file, data_file].each do |file|
+      name = File.basename(file)
+      directory.files.create(:key => name, :body => open(file))
+      directory.files.create(:key => "latest#{File.extname(file)}", :body => open(file))
+    end
+  else
+    $stderr.puts 'ERROR: No remote directory defined for backup storage!'
+    exit -1
+  end
+else
+  puts 'WARN: No remote credentials found. Backup is local only!'
+end
+
+puts 'SUCCESS: Backup is complete!'
