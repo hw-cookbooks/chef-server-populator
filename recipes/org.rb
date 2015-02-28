3c9d39da5a5ddff17eebde9d0dd9a63298c4fa51
Michael F. Weinberg <michael@luckymike.com>
2015-02-28 10:54:09 -0800

    Force a reconfigure, to get the server in a configurable state.

--- recipes/org.rb
+++ recipes/org.rb
@@ -2,6 +2,10 @@ org = node[:chef_server_populator][:populator_org]
 
 directory org[:validator_dir]
 
+chef_server_ingredient 'chef-server-core' do
+  action :reconfigure
+end
+
 execute 'create populator org' do
   command "chef-server-ctl org-create #{org[:name]} #{org[:full_name]} -f #{File.join(org[:validator_dir],org[:name])}-validator.pem"
 end
