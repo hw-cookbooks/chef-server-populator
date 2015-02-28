8f981dd85cea4822488dbc60f5e321ebbccdf49e
Michael F. Weinberg <michael@luckymike.com>
2015-02-27 17:06:46 -0800

    include org recipe

---
+++ recipes/org.rb
@@ -0,0 +1,11 @@
+org = node[:chef_server_validator][:populator_org]
+
+directory org[:validator_dir]
+
+execute 'create populator org' do
+  command "chef-server-ctl org create #{org[:name]} #{org[:full_name]} -f #{org[:validator_dir]}"
+end
+
+# execute 'update populator org validator' do
+#   command "
+# end
