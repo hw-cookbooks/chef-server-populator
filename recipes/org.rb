c2d5f83ab55f904d1f713cc460c4b356bb262b46
Michael F. Weinberg <michael@luckymike.com>
2015-02-27 17:21:24 -0800

    fix org create command name

--- recipes/org.rb
+++ recipes/org.rb
@@ -3,7 +3,7 @@ org = node[:chef_server_populator][:populator_org]
 directory org[:validator_dir]
 
 execute 'create populator org' do
-  command "chef-server-ctl org create #{org[:name]} #{org[:full_name]} -f #{org[:validator_dir]}"
+  command "chef-server-ctl org-create #{org[:name]} #{org[:full_name]} -f #{org[:validator_dir]}"
 end
 
 # execute 'update populator org validator' do
