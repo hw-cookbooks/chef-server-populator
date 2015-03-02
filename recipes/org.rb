be0b0c6bfd31a12ca8891039bf94bb1511af336d
Michael F. Weinberg <michael@luckymike.com>
2015-03-02 15:06:37 -0800

    Use hot new built in commands to set the populator org validator key, remove the default and don't save the generated pem anywhere.

--- recipes/org.rb
+++ recipes/org.rb
@@ -1,14 +1,13 @@
 org = node[:chef_server_populator][:populator_org]
 
-directory org[:validator_dir]
-
 execute 'create populator org' do
-  command "chef-server-ctl org-create #{org[:name]} #{org[:full_name]} -f #{File.join(org[:validator_dir],org[:name])}-validator.pem"
+  command "chef-server-ctl org-create #{org[:name]} #{org[:full_name]}"
 end
 
-execute 'update populator org validator' do
-  command "/opt/opscode/embedded/bin/psql -d opscode_chef -c \"update clients set public_key=E'#{File.read(node[:chef_server_populator][:populator_org][:validator_pub_key])}' where name='#{org[:name]}-validator';\""
-  user 'opscode-pgsql'
-  not_if %Q(sudo -i -u opscode-pgsql #{pg_cmd} -c "select name from clients where name = '#{client}' and public_key = E'#{pub_key.gsub("\n", '\\n')}'" -tq | tr -d ' ' | grep '^#{client}$')
+execute 'add populator org validator key' do
+  command "chef-server-ctl add-client-key #{org[:name]} #{org[:name]}-validator #{org[:validator_pub_key]} --key-name populator"
 end
 
+execute 'remove populator org default validator key' do
+  command "chef-server-ctl delete-client-key #{org[:name]} #{org[:name]}-validator default"
+end
