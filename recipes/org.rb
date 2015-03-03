80fcd34b717df8b7d18a83d511f7d40e01159776
Michael F. Weinberg <michael@luckymike.com>
2015-03-03 10:15:21 -0800

    Got a little excited with the copy-pasta

--- recipes/org.rb
+++ recipes/org.rb
@@ -7,12 +7,12 @@ end
 
 execute 'add populator org validator key' do
   command "chef-server-ctl add-client-key #{org[:name]} #{org[:name]}-validator #{org[:validator_pub_key]} --key-name populator"
-  not_if "chef-server-ctl list-client-keys #{org[:name]} #{org[:name]}-validator #{org[:validator_pub_key]} | grep '^key_name: populator$'"
+  not_if "chef-server-ctl list-client-keys #{org[:name]} #{org[:name]}-validator | grep '^key_name: populator$'"
 end
 
 execute 'remove populator org default validator key' do
   command "chef-server-ctl delete-client-key #{org[:name]} #{org[:name]}-validator default"
-  only_if "chef-server-ctl list-client-keys #{org[:name]} #{org[:name]}-validator #{org[:validator_pub_key]} | grep '^key_name: default$'"
+  only_if "chef-server-ctl list-client-keys #{org[:name]} #{org[:name]}-validator | grep '^key_name: default$'"
 end
 
 node.set['chef-server'][:configuration][:default_orgname] = node[:chef_server_populator][:populator_org][:name]
