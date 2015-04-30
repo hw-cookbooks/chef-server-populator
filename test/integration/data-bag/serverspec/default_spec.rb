b3afb90869541ca684067e86201235ad166d293b
Michael F. Weinberg <michael@hw-ops.com>
2015-04-29 17:07:37 -0700

    Add test converage for data bag items/client recipe.

---
+++ test/integration/data-bag/serverspec/default_spec.rb
@@ -0,0 +1,9 @@
+require_relative './spec_helper'
+
+# describe 'chef-server-populator-cookbook-upload' do
+
+#   describe command('knife cookbook list -c /etc/opscode/pivotal.rb -s https://localhost/organizations/inception_llc') do
+#     its(:stdout) { should match /chef-server-populator/ }
+#   end
+
+# end
