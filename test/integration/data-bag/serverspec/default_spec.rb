4097a1459387f01522a8855c1179a02674b9ade3
Michael F. Weinberg <michael@hw-ops.com>
2015-05-04 11:30:00 -0700

    remove commented test stub

--- test/integration/data-bag/serverspec/default_spec.rb
+++ test/integration/data-bag/serverspec/default_spec.rb
@@ -1,9 +1,1 @@
 require_relative './spec_helper'
-
-# describe 'chef-server-populator-cookbook-upload' do
-
-#   describe command('knife cookbook list -c /etc/opscode/pivotal.rb -s https://localhost/organizations/inception_llc') do
-#     its(:stdout) { should match /chef-server-populator/ }
-#   end
-
-# end
