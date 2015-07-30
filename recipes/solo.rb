de7e965fc3ffe2a8dd8f62076397ff4cc7d88ef2
Chris Roberts <code@chrisroberts.org>
2015-07-30 13:25:40 -0700

    Fix file extension when fetching latest from remote store

--- recipes/solo.rb
+++ recipes/solo.rb
@@ -13,9 +13,9 @@ if(node[:chef_server_populator][:backup][:remote][:connection])
   remote_directory = node[:chef_server_populator][:backup][:remote][:directory]
   remote = Miasma.api(:provider => remote_creds[:provider].to_s.downcase, :type => 'storage', :credentials => remote_creds[:credentials])
   remote_bucket = remote.buckets.get(remote_directory)
-  if(remote_bucket && gz_file = remote_bucket.files.get(File.join(node[:chef_server_populator][:backup][:remote][:file_prefix], 'latest.gz')))
+  if(remote_bucket && gz_file = remote_bucket.files.get(File.join(node[:chef_server_populator][:backup][:remote][:file_prefix], 'latest.tgz')))
     dump_file = remote_bucket.get(remote_bucket.files.get(File.join(node[:chef_server_populator][:backup][:remote][:file_prefix], 'latest.dump')))
-    local_gz = '/tmp/latest.gz'
+    local_gz = '/tmp/latest.tgz'
     local_dump = '/tmp/latest.dump'
     File.open(local_gz, 'wb') do |file|
       while(data = gz_file.body.readpartial(2048))
