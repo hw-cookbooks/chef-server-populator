2b35ca02220708c9f12dade91d5c9afaf4b64ad6
Chris Roberts <code@chrisroberts.org>
2015-12-31 08:02:37 -0800

    Update cookbook to support chef-server 4.x
    The chef-server 4.0 cookbook introduced a breaking change around
    configuration setup by converting a previously Hash typed attribute to
    an inline string. Added support for this style of configuration and
    removed usage of no longer existing resources for triggering reconfiguration.

--- recipes/org.rb
+++ recipes/org.rb
@@ -1,7 +1,6 @@
 include_recipe 'chef-server'
 
 conf_dir = node[:chef_server_populator][:base_path]
-node.set['chef-server'][:configuration][:default_orgname] = node[:chef_server_populator][:default_org]
 
 org = node[:chef_server_populator][:solo_org]
 user = node[:chef_server_populator][:solo_org_user]
@@ -26,11 +25,16 @@ execute 'delete default user key' do
   only_if "chef-server-ctl list-user-keys #{user[:name]} | grep 'name: default$'"
 end
 
+execute 'reconfigure for populator org create' do
+  command 'chef-server-ctl reconfigure'
+  action :nothing
+end
+
 execute 'create populator org' do
   command "chef-server-ctl org-create #{org[:org_name]} #{org[:full_name]} -a #{user[:name]}"
   not_if "chef-server-ctl org-list | grep '^#{org[:org_name]}$'"
   if org[:org_name] == node[:chef_server_populator][:default_org]
-    notifies :reconfigure, 'chef_server_ingredient[chef-server-core]', :immediately
+    notifies :run, 'execute[reconfigure for populator org create]', :immediately
   end
 end
 
