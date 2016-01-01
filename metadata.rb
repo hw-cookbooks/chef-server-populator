faf345d11d13ce587acb0fe3074f4f6df781899b
Chris Roberts <code@chrisroberts.org>
2015-12-31 16:20:50 -0800

    Add explicit constraint to chef-server dependency

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name 'chef-server-populator'
 description 'Populate chef server with stuff you want'
 maintainer 'Heavywater'
 maintainer_email 'support@hw-ops.com'
-version '0.4.2'
+version '0.4.4'
 
-depends 'chef-server', '>= 2.1.0'
+depends 'chef-server', '2.1.4'
