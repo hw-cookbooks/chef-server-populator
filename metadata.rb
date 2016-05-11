8ae7794f0d58586ff1945e7ca03a3e955c9ede70
Michael F. Weinberg <michael@luckymike.com>
2016-05-11 14:07:26 -0700

    Increment version for development

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name 'chef-server-populator'
 description 'Populate chef server with stuff you want'
 maintainer 'Heavywater'
 maintainer_email 'support@hw-ops.com'
-version '2.0.2'
+version '2.0.3'
 
 depends 'chef-server', '~> 5.0'
